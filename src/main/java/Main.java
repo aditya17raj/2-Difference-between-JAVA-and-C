5d31d21b49e25c77d71e4910331c25aa91bc4f17
Aditya <141512093+aditya17raj@users.noreply.github.com>
2024-04-10 14:22:17 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,25 @@
+CPP
+  
+  *It is platform dependent.
+  *It supports goto statement,operator     
+   overloading,pointer and multiple inheritance.
+  *It uses a compiler.
+  *It supports call by value,call by reference,call by 
+   pointer and call by reference to pointer.
+  *Runtime error detection mechanism isnthe 
+   responsibility of the programmer.
+  *It allows procedural as well as object oriented 
+   programming.
+  *It has new and delete keyword for object management.
+
+JAVA 
+
+  *It is platform independent.
+  *It does not support goto statement,operator 
+   overloading,pointer and multiple inheritance.
+  *It uses compiler and interpreter.
+  *Runtime errpr detection mechanism is the 
+   responsibility of the system.
+  *It allows only object oriented programming.
+  *It has automatic garbage collection for object 
+   management.
